ed9114f59bea59a739363cfdb186d6194a7b3513
RainbowMango <qdurenhongcai@163.com>
2014-12-29 23:08:27 +0800

    draw view with autolayout

--- UIImagePickerControllerDemo/UIImagePickerControllerDemo/ShowImageViewController.swift
+++ UIImagePickerControllerDemo/UIImagePickerControllerDemo/ShowImageViewController.swift
@@ -10,16 +10,16 @@ import UIKit
 
 class ShowImageViewController: UIViewController, UIScrollViewDelegate {
 
-    var imageView: UIImageView!
-    var scrollView: UIScrollView!
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var imageView: UIImageView!
     
     var image: UIImage!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        setScrollView()
         setImageView()
+        setScrollView()
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,19 +28,19 @@ class ShowImageViewController: UIViewController, UIScrollViewDelegate {
     }
     
     func setImageView() {
-        self.imageView = UIImageView(frame: self.view.frame)
-        self.imageView.contentMode = UIViewContentMode.ScaleAspectFit
+        //self.imageView.frame = self.view.frame
+        //self.imageView.contentMode = UIViewContentMode.ScaleAspectFit
         self.imageView.image = self.image
-        self.scrollView.addSubview(self.imageView)
+        //self.scrollView.addSubview(self.imageView)
     }
     
     func setScrollView() {
-        self.scrollView = UIScrollView(frame: self.view.bounds)
-        self.scrollView.contentSize = self.image.size
+        //self.scrollView.frame = self.view.frame
+        //self.scrollView.contentSize = self.view.frame.size
         self.scrollView.delegate = self
-        self.scrollView.minimumZoomScale = 0.5
-        self.scrollView.maximumZoomScale = 5
-        self.view.addSubview(self.scrollView)
+        //self.scrollView.minimumZoomScale = 0.5
+        //self.scrollView.maximumZoomScale = 5
+        //self.view.addSubview(self.scrollView)
     }
     
     func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView? {
